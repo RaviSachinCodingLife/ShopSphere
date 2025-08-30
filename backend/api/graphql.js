bd4d0a4551b9449322235c01a85f05a7a6c367ce
RaviSachinCodingLife <sachinsaurabh.msbcgroup@gmail.com>
2025-08-30 09:30:05 +0530

    fixes

--- backend/api/graphql.js
+++ backend/api/graphql.js
@@ -1,12 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { readFileSync } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
 import { resolvers } from "../resolvers.js";
-import { gql } from "graphql-tag";
-import schemaString from "../schema.graphql?raw"; // <- Vercel-friendly
 
-const typeDefs = gql`
-  ${schemaString}
-`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, "..");
+const typeDefs = readFileSync(join(__dirname, "schema.graphql"), "utf8");
 
 const server = new ApolloServer({
   typeDefs,
